8d3a833829826468128b5082f59d461ccd8ebe35
Mithun <102860039+Mithun3110@users.noreply.github.com>
2025-06-11 15:21:12 +0530

    spring setup

---
+++ src/main/java/com/omini/ai/Spring_Boot_Weaviate/SpringBootWeaviateApplication.java
@@ -0,0 +1,13 @@
+package com.omini.ai.Spring_Boot_Weaviate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeaviateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeaviateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omini/ai/Spring_Boot_Weaviate/SpringBootWeaviateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omini.ai.Spring_Boot_Weaviate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeaviateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
